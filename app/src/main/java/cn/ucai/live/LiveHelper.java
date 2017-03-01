7ae544e7c5718b5d2099db3fe2ea2b3b3b70703c
blackfox <huangy_tao@126.com>
2017-03-01 10:53:51 +0800

    LiveHelper中添加asyncGetCurrentUserInfo()方法

--- app/src/main/java/cn/ucai/live/LiveHelper.java
+++ app/src/main/java/cn/ucai/live/LiveHelper.java
@@ -52,6 +52,7 @@ import cn.ucai.live.data.local.LiveDBManager;
 import cn.ucai.live.data.local.UserDao;
 import cn.ucai.live.data.model.Result;
 import cn.ucai.live.ui.activity.ChatActivity;
+import cn.ucai.live.ui.activity.LoginActivity;
 import cn.ucai.live.ui.activity.MainActivity;
 import cn.ucai.live.utils.L;
 import cn.ucai.live.utils.OnCompleteListener;
@@ -60,6 +61,34 @@ import cn.ucai.live.utils.ResultUtils;
 
 
 public class LiveHelper {
+    public void asyncGetCurrentUserInfo(LoginActivity activity) {
+        L.e("UserProfileManager","asyncGetCurrentUserInfo,username="+EMClient.getInstance().getCurrentUser());
+        NetDao.getUserInfoByUsername(activity, EMClient.getInstance().getCurrentUser(), new OnCompleteListener<String>() {
+            @Override
+            public void onSuccess(String s) {
+                L.e("UserProfileManager","s="+s);
+                if (s!=null){
+                    Result result = ResultUtils.getResultFromJson(s, User.class);
+                    if (result!=null && result.isRetMsg()){
+                        User user = (User) result.getRetData();
+                        L.e(TAG,"user="+user);
+                        if (user!=null) {
+                            //save user info to db
+                            LiveHelper.getInstance().saveAppContact(user);
+                            PreferenceManager.getInstance().setCurrentUserNick(user.getMUserNick());
+                            PreferenceManager.getInstance().setCurrentUserAvatar(user.getAvatar());
+                        }
+                    }
+                }
+            }
+
+            @Override
+            public void onError(String error) {
+                L.e("UserProfileManager","error="+error);
+            }
+        });
+    }
+
     /**
      * data sync listener
      */
