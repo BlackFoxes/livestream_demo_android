8d8a342064f8ec46ad350fac7f381377be609875
blackfox <huangy_tao@126.com>
2017-03-06 10:08:17 +0800

    直播的观众头像的显示

--- app/src/main/java/cn/ucai/live/ui/activity/LiveBaseActivity.java
+++ app/src/main/java/cn/ucai/live/ui/activity/LiveBaseActivity.java
@@ -503,12 +503,17 @@ public abstract class LiveBaseActivity extends BaseActivity {
         @Override public void onClick(View v) {
           showUserDetailsDialog(namelist.get(position));
         }
+
       });
-      //暂时使用测试数据
-      Glide.with(context)
-          .load(avatarRepository.getAvatar())
-          .placeholder(R.drawable.ease_default_avatar)
-          .into(holder.Avatar);
+      EaseUserUtils.setAppUserAvatar(context,namelist.get(position),holder.Avatar);
+
+
+
+//      //暂时使用测试数据
+//      Glide.with(context)
+//          .load(avatarRepository.getAvatar())
+//          .placeholder(R.drawable.ease_default_avatar)
+//          .into(holder.Avatar);
     }
 
     @Override public int getItemCount() {
